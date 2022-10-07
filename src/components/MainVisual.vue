8643f7e755258220d35ea18f5d318d4c033638ee
kimhaein0809 <laura0809@naver.com>
2022-10-07 12:18:34 +0900

    nn

--- src/components/MainVisual.vue
+++ src/components/MainVisual.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="visual"></div>
+  <div class="visual">
+    <Swiper>
+      <SwiperSlide>test1</SwiperSlide>
+      <SwiperSlide>test2</SwiperSlide>
+      <SwiperSlide>test3</SwiperSlide>
+    </Swiper>
+  </div>
 </template>
 
 <script>
+  import { Swiper,SwiperSlide } from "swiper/vue";
+  import "swiper/css"
 export default {
-
+  components:{
+    Swiper,
+    SwiperSlide,
+  }
 }
 </script>
-
+  
 <style lang="scss">
     .visual{
         height: 300px;
