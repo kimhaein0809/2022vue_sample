4b645d2ce9484f5465f15aefb66ff656689c6185
kimhaein0809 <laura0809@naver.com>
2022-10-12 13:37:45 +0900

    df

--- src/components/MainVisual.vue
+++ src/components/MainVisual.vue
@@ -1,25 +1,32 @@
 <template>
-  <swiper 
-  :navigation="true" 
-  :modules="modules" 
-  :loop="true"
-  :pagination="{
-      clickable: true,
-    }"
-  class="mySwiper"
-  >
-    <swiper-slide 
-      v-for="(item,i) in swiperView" 
-      :key="item" 
-      :style="{'background-color':`${swiperView[i].color}`}"
+  <div class="visual">
+    <swiper
+    :navigation="true"
+    :modules="modules"
+    v-bind="swiperOptions"
+    :loop="true"
+    :pagination="{
+        clickable: true,
+      }"
+    class="mySwiper"
     >
-      <!-- <div :style="{'background-color':`${item.color}`}" class="innerItem"> -->
-      <div class="innerItem">
-        <img :src="`./images/swiper/${item.src}`" alt="">
-      </div>
-    </swiper-slide>
+      <swiper-slide
+        v-for="(item,i) in swiperView"
+        :key="item"
+        :style="{'background-color':`${swiperView[i].color}`}"
+      >
+        <!-- <div :style="{'background-color':`${item.color}`}" class="innerItem"> -->
+        <div class="innerItem">
+          <img :src="`./images/swiper/${item.src}`" alt="">
+        </div>
+      </swiper-slide>
     
-  </swiper>
+    </swiper>
+    <div class="btnWrap">
+      <div class="prev">이전</div>
+      <div class="next">다음</div>
+    </div>
+  </div>
 </template>
 <script>
 // Import Swiper Vue.js components
@@ -47,6 +54,16 @@ export default {
     return {
       modules: [Navigation,Pagination],
       swiperView:sData,
+      swiperOptions:{
+        loop:true,
+        navigation:{
+          nextEl:'.visual .next',
+          prevEl:'.visual .prev',
+        },
+        pagination:{
+          type:'fraction'
+        }
+      }
     };
   },
 };
@@ -61,6 +78,24 @@ export default {
       img{
         display: block;
         margin:0 auto
+      }
+    }
+  }
+  .visual{
+    position: relative;
+    .btnWrap{
+      gap:10px;
+      display: flex;
+      position: absolute;
+      bottom: 20px;
+      z-index: 10;
+      left: calc( 50% + 300px );
+      .next,.prev{
+        padding: 5px 10px;
+        background: rgba(0,0,0,0.3);
+        border-radius: 10px;
+        color: white;
+        cursor: pointer;
       }
     }
   }
